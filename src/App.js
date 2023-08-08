23bbc6bc0b3768a3a33945a33cd1fd0203e1bc5f
balavedhaa <94523760+balavedhaa@users.noreply.github.com>
2023-08-08 21:21:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import { Route,Routes, BrowserRouter } from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
-import Navigati from './components/Navigation';
-import Navbar from './components/NavLayout';
-import Blog from './components/Blog';
-import Apps from './components/ReactApp';
+import * as React from 'react';
+// import tony from "./tony.jpeg";
+// import aidslogo from "./CYAN_PNG.png"
+// import bg from "./bg.jpg"
+// import { render } from '@testing-library/react';
+// import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from './Components/Layout.js';  
+import Resume from './Components/Resume.js';
+import Projects from './Components/Projects.js';
+import Profile from './Components/Profile.js';
 
 function App() {
+
+  // const [show,setshow]=useState(false)
+
+  // function myfunhome1(){
+  //   setshow(!show)
+  // }
   return (
-    <BrowserRouter>
+    <div>
+      <BrowserRouter>
       <Routes>
-      <Route path="/" element={<Navbar />} >
-          <Route index element={<Navigati />} />
-          <Route path='blog' element ={<Blog />} />
-          <Route path='reactApp' element ={<Apps />} />
+        <Route path="/" element={<Layout />}>
+        <Route index element={<Profile />} /> 
+        <Route path="Resume"  element={<Resume />} />
+        <Route path="Projects" element={<Projects />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+      </BrowserRouter>
+
+    </div>
   );
-}
+};
+
 
 export default App;
